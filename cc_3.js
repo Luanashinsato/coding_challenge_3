edf023a2a82c20b7e8b834095093db06c2cd35ba
Luanashinsato <lshinsato@usf.edu>
2025-01-30 18:02:18 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// Task 1: Product Price Management Scenario
+let prices = [12, 20, 28, 32, 35]; // Declaring an array with initial prices
+prices.push (40); // Adding a new price to the array
+prices.shift(); // Removing the first price from the array
+console.log("Updated Price List:", prices); // Logging the updated price list
